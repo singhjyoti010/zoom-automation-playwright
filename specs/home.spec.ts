942125a86b371ab3c47531e231321702978c157c
Jyoti Singh <singhjyoti010@outlook.com>
2024-10-27 14:28:53 +0530

    updated home.spec.ts

--- specs/home.spec.ts
+++ specs/home.spec.ts
@@ -7,12 +7,13 @@ let loginPage: LoginPage;
 const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 
+test.beforeEach("create page context", async ({page}) => {
+    homePage = new HomePage(page);
+    loginPage = new LoginPage(page);
+    await loginPage.login(email, password);
+})
+
 test.describe('Home Page validations', () => {
-    test.beforeEach("create page context", async ({page}) => {
-        homePage = new HomePage(page);
-        loginPage = new LoginPage(page);
-        await loginPage.login(email, password);
-    })
 
     test('verify after status of pages in left nav', async ({page}) => {
         await homePage.isActiveHome();
@@ -31,21 +32,4 @@ test.describe('Home Page validations', () => {
         await homePage.isActiveDataAndPrivacy();
         await homePage.isActiveReports();
     })
-    // test.afterEach(async ({ page }, testInfo) => {
-    //     if (testInfo.status === 'failed') {
-    //       // Define the path where the screenshot will be saved
-    //       const screenshotPath = path.join('screenshots', `${testInfo.title.replace(/\s+/g, '_')}-${new Date().toISOString()}.png`);
-          
-    //       // Create the screenshots directory if it doesn't exist
-    //     //   if (!fs.existsSync('screenshots')) {
-    //     //     fs.mkdirSync('screenshots');
-    //     //   }
-          
-    //       // Capture and save the screenshot
-    //       await page.screenshot({ path: screenshotPath });
-          
-    //       // Optionally, you can log the path of the screenshot
-    //       console.log(`Screenshot saved at ${screenshotPath}`);
-    //     }
-    // });
 })
