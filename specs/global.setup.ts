361fc91a035fb531c9ce229bb41489fc95de32e7
Jyoti Singh <singhjyoti010@outlook.com>
2024-11-03 23:15:13 +0530

    global-setup add

---
+++ specs/global.setup.ts
@@ -0,0 +1,13 @@
+import { test as setup } from '@playwright/test';
+import { LoginPage } from "../pageobjects/login.page";
+
+let loginPage: LoginPage;
+const email = process.env.USER_EMAIL!;
+const password = process.env.USER_PASSWORD!;
+
+setup('login setup', async ({page }) => {
+    console.log('logging in...');
+    loginPage = new LoginPage(page);
+    await loginPage.login(email, password);
+    await page.context().storageState({ path: ".auth/user.json" })
+});
