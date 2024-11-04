b56d3cb52201b1e3106730738753026d47cc9267
Jyoti Singh <singhjyoti010@outlook.com>
2024-11-04 22:03:46 +0530

    removed unused line of code from schedule-spec

--- specs/schedule.spec.ts
+++ specs/schedule.spec.ts
@@ -1,25 +1,18 @@
 import { test } from "@playwright/test";
-import { LoginPage } from "../pageobjects/login.page";
 import { HomePage } from "../pageobjects/home.page";
 import { ScheduleMeetingPage } from "../pageobjects/schedule.page";
 
-let loginPage: LoginPage;
 let homePage: HomePage;
 let scheduleMeeting: ScheduleMeetingPage;
-const email = process.env.USER_EMAIL!;
-const password = process.env.USER_PASSWORD!;
 
 test.beforeEach("", async ({page}) => {
-    // loginPage = new LoginPage(page);
     homePage = new HomePage(page);
     scheduleMeeting = new ScheduleMeetingPage(page);
-    // await loginPage.login(email, password);
     await page.goto('https://us05web.zoom.us/myhome');
     await homePage.isActiveMeetings();
 })
 
 test.describe("Schedule Meeting test", () => {
-
     test("verifying meeting creation", async () =>{
         await scheduleMeeting.createMeeting("Test meeting");
     })
