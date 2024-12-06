9d238dd3298f822db1e5bc66380c69bf033b1c27
Jyoti Singh <singhjyoti010@outlook.com>
2024-12-07 00:27:06 +0530

    updated test title-01

--- specs/loginspecs/01.spec.ts
+++ specs/loginspecs/01.spec.ts
@@ -5,8 +5,8 @@ const email = process.env.USER_EMAIL!;
 const password = process.env.USER_PASSWORD!;
 let loginPage: LoginPage;
 
-test.describe("Zoom signin", () => {
-    test('login', async ({ page }) => {
+test.describe("[successful Authentication] Verify user signin with email/password", () => {
+    test('user login with correct credentials', async ({ page }) => {
         await page.context().clearCookies();
         loginPage = new LoginPage(page);
         await loginPage.login(email, password);
