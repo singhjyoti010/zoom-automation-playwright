63be5570cbfc7ec0e19d1359b112c0677305b32b
Jyoti Singh <singhjyoti010@outlook.com>
2024-12-06 16:57:41 +0530

    adding more login validation

--- pageobjects/login.page.ts
+++ pageobjects/login.page.ts
@@ -32,6 +32,22 @@ export class LoginPage {
         return this.page.locator('.ot-floating-button__open');
     }
 
+    public get emptyEmailMessage() {
+        return this.page.locator('//*[contains(@aria-label,"Please enter your email address")]')
+    }
+
+    public get inocrrectEmailFormatMessage() {
+        return this.page.locator('//*[contains(@aria-label,"Please enter a valid email address.")]');
+    }
+
+    public get incorrectEmailPasswordMsg() {
+        return this.page.locator('//*[contains(@class,"zm-alert--error")]//p[contains(text(),"Incorrect email or password")]');
+    }
+
+    public get emptyPasswordMessage() {
+        return this.page.locator('//*[contains(@aria-label,"Please enter your password")]');
+    }
+
     
     /******************************** helper methods ****************************************/
 
@@ -40,26 +56,30 @@ export class LoginPage {
      * @param email user email
      * @param password user password
     */
-
-    public async enterEmailPass(email: string, password: string) {
-        //fill email field
-        await this.page.locator(this.emailInputFld).type(email);
-        // await this.emailInputField.type(email);
-        //fill password field
-        await this.passwordInputField.type(password);
-    }
-
-    public async login(email: string, password: string) {
+    public async gotoSignInPage(){
         //go to zoom signin page
         await this.page.goto('https://zoom.us/signin');
         await this.page.waitForLoadState();
         await this.page.waitForTimeout(5000);
         //verify page title
         await expect(this.page).toHaveTitle(/Sign In/);
-
         //wait for cookies banner to appear
         await this.bannerCloseButton.waitFor({state: "visible", timeout: 40000});
         await this.bannerCloseButton.click();
+    }
+
+    public async enterEmailPass(email: string, password: string) {
+        //fill email field
+        await this.emailInputField.clear();
+        await this.page.locator(this.emailInputFld).type(email);
+        // await this.emailInputField.type(email);
+        //fill password field
+        await this.passwordInputField.clear();
+        await this.passwordInputField.type(password);
+    }
+
+    public async login(email: string, password: string) {
+        await this.gotoSignInPage();
         await this.enterEmailPass(email, password);
 
         // //wait for preference button to appear
@@ -83,5 +103,31 @@ export class LoginPage {
 
         //wait for user name post login
         await this.page.waitForSelector('//h3[contains(text(),"Emma Grace")]', { state: 'visible', timeout: 80000 });
+    }
+    async emptyEmail(email: string, password: string) {
+        // await this.gotoSignInPage();
+        await this.enterEmailPass(email,password);
+        await this.signinButton.click();
+        await expect(this.emptyEmailMessage).toBeVisible();
+        await expect(this.emptyEmailMessage).toContainText('Please enter your email address');
+    }
+    async checkIncorrectEmailFormat(email: string, password: string){
+        // await this.gotoSignInPage();
+        await this.enterEmailPass(email,password);
+        await this.signinButton.click();
+        await expect(this.inocrrectEmailFormatMessage).toBeVisible();
+        await expect(this.inocrrectEmailFormatMessage).toContainText('Please enter a valid email address.');
+    }
+    async incorrectEmail(email: string, password: string){
+        await this.enterEmailPass(email,password);
+        await this.signinButton.click();
+        await expect(this.incorrectEmailPasswordMsg).toBeVisible();
+        await expect(this.incorrectEmailPasswordMsg).toContainText('Incorrect email or password');
+    }
+    async emptyPassword(email: string, password: string) {
+        await this.enterEmailPass(email,password);
+        await this.signinButton.click();
+        await expect(this.emptyPasswordMessage).toBeVisible();
+        await expect(this.emptyPasswordMessage).toContainText('Please enter your password');
     }
 }
