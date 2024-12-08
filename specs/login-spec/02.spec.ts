882d7a57de07539d5f1f21cd9e73ebe4679a5105
Jyoti Singh <singhjyoti010@outlook.com>
2024-12-08 09:53:13 +0530

    re-adding login specs

---
+++ specs/login-spec/02.spec.ts
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+import { LoginPage } from "../../pageobjects/login.page";
+import { helperConstants } from "../../utils/helperConstants";
+
+const email = process.env.USER_EMAIL!;
+const password = process.env.USER_PASSWORD!;
+let loginPage: LoginPage;
+
+test.describe("[Negative Test] Verify error details when user signs in with email/password-incorrect credentials", () => {
+    test.beforeEach('test pre-requisites', async ({ page }) => {
+        await page.context().clearCookies();
+        loginPage = new LoginPage(page);
+        await loginPage.gotoSignInPage();
+    })
+
+    test('Enter incorrect email format with empty password field', async ({ page }) => {
+        await loginPage.checkIncorrectEmailFormat(helperConstants.incorrectEmailFormat, helperConstants.emptyFields);
+        await loginPage.emptyPassword(helperConstants.incorrectEmailFormat, helperConstants.emptyFields);
+    })
+    test('Enter incorrect email format with non-empty password field', async ({ page }) => {
+        await loginPage.checkIncorrectEmailFormat(helperConstants.incorrectEmailFormat, helperConstants.emptyFields);
+    })
+    test('Empty email with non-empty password field', async ({ page }) => {
+        await loginPage.emptyEmail(helperConstants.emptyFields, helperConstants.incorrectPassword);
+    })
+    test('Incorrect email/password', async ({ page }) => {
+        await loginPage.incorrectEmail(helperConstants.incorrectEmail, password);
+    })
+})
