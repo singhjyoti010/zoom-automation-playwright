edf364bba03b0d2d804778c95c1ba3972e943863
Jyoti Singh <singhjyoti010@outlook.com>
2024-12-10 15:07:40 +0530

    fixed exclusivity of 03.spec.ts

--- specs/homespecs/03.spec.ts
+++ specs/homespecs/03.spec.ts
@@ -3,19 +3,24 @@ import { HomePage } from "../../pageobjects/home.page";
 
 let homePage: HomePage;
 
-test.beforeEach("create page context", async ({browser}) => {
-    const context = await browser.newContext({
-        storageState: '.auth/user.json', // Use the path to your saved auth.json file
-    });
-    const page = await context.newPage();
-    homePage = new HomePage(page);
-    await page.goto('https://us05web.zoom.us/myhome');
-    await page.waitForLoadState();
-})
+// test.beforeEach("create page context", async ({browser}) => {
+//     const context = await browser.newContext({
+//         storageState: '.auth/user.json', // Use the path to your saved auth.json file
+//     });
+//     const page = await context.newPage();
+//     homePage = new HomePage(page);
+//     await page.goto('https://us05web.zoom.us/myhome');
+//     await page.waitForLoadState();
+// })
 
 test.describe('Verify user can navigate to non-admin tiles present in left rail (Home, Profile, Meetings, Webinars, Events & Sessions, Personal Contacts, Personal Devices, Whiteboards, Notes, Docs, Recording, Clips, Scheduler, Settings, Data & Privacy, and Reports)', () => {
-    test('PERSONAL -> Home', async ({page}) => {
+    test.beforeEach("create page context", async ({page}) => {
+        homePage = new HomePage(page);
         await page.goto('https://us05web.zoom.us/myhome');
+        await page.waitForLoadState();
+    })
+    test('PERSONAL -> Home', async ({page}) => {
+        // await page.goto('https://us05web.zoom.us/myhome');
         await page.waitForLoadState();
         await homePage.isActiveHome();
     })
@@ -43,7 +48,7 @@ test.describe('Verify user can navigate to non-admin tiles present in left rail 
     test('PERSONAL -> Notes', async ({page}) => {
         await homePage.isActiveNotes();
     })
-    test.only('PERSONAL -> Docs', async ({page}) => {
+    test('PERSONAL -> Docs', async ({page}) => {
         await homePage.docNavigation();
     })
     test('PERSONAL -> Recording', async ({page}) => {
