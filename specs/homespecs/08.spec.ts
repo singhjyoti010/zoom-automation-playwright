65c0477fda40ce99799264a5c644227422fef877
Jyoti Singh <singhjyoti010@outlook.com>
2024-12-20 16:46:48 +0530

    updating to beforeAll from beforeEach for reduendant pre-test

--- specs/homespecs/08.spec.ts
+++ specs/homespecs/08.spec.ts
@@ -2,9 +2,13 @@ import test from "@playwright/test";
 import { HomePage } from "../../pageobjects/home.page";
 
 let homePage: HomePage;
+let page;
+let context;
 
 test.describe("[Zoom-Home] Your Activity Default view, verify if no activity is performed, every tile details should have Learn more and Create [Activity_Name] button present along with Visit [Activity_Name] link.", () => {
-    test.beforeEach("initial setup", async ({page}) => {
+    test.beforeAll("initial setup", async ({browser}) => {
+        context = await browser.newContext();
+        page = await context.newPage();
         homePage = new HomePage(page);
         await page.goto('https://us05web.zoom.us/myhome');
         await page.waitForLoadState();
